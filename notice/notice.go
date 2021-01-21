2d643fd6d32491c65636325fb894d82db94b52cc
aucuba <aoki.070207@gmail.com>
2021-01-21 11:26:08 +0900

    Update notice.go

--- notice/notice.go
+++ notice/notice.go
@@ -1,9 +1,16 @@
 package notice
 
 import (
+	"path/filepath"
+
+	"fmt"
+
 	"github.com/martinlindhe/notify"
 )
 
-func Pop(appName string, title string, text string, imagePath string) {
-	notify.Notify(appName , title, text , imagePath);
+var imagePath, _ = filepath.Abs("neko.png")
+
+func Pop(appName string, title string, text string) {
+	fmt.Println(title, text)
+	notify.Notify(appName, title, text, imagePath)
 }

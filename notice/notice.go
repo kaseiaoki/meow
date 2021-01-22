8cdbbf1eceef6ae40d5bc5b0eb8840277075377a
aucuba <aoki.070207@gmail.com>
2021-01-22 13:59:22 +0900

    Update notice.go

--- notice/notice.go
+++ notice/notice.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"path/filepath"
+	"runtime"
 
 	"github.com/kyokomi/emoji"
+	"github.com/martinlindhe/notify"
 	"gopkg.in/toast.v1"
 )
 
@@ -13,16 +15,18 @@ var imagePath, _ = filepath.Abs("./img/neko.png")
 
 func Pop(appName string, title string, text string, endless bool) {
 	fmt.Println(title+emoji.Sprint(":cat2:"), text)
-
-	notification := toast.Notification{
-		AppID:   appName,
-		Title:   title,
-		Message: text,
-		Icon:    imagePath, // This file must exist (remove this line if it doesn't)
-		Loop:    endless,
+	if runtime.GOOS == "windows" {
+		notification := toast.Notification{
+			AppID:   appName,
+			Title:   title,
+			Message: text,
+			Icon:    imagePath, // This file must exist (remove this line if it doesn't)
+			Loop:    endless,
+		}
+		err := notification.Push()
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
-	err := notification.Push()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	notify.Notify(appName, title, text, imagePath)
 }

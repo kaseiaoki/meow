7efa2761f6047335a2605aead0311139b344ec03
aucuba@kaseiaoki <aoki.070207@gmail.com>
2021-01-26 20:07:23 +0900

    help

--- cmd/root.go
+++ cmd/root.go
@@ -44,8 +44,25 @@ func newRootCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "mw",
 		Short: "meow! this is notifer",
-		Long: `meow!
-		 mh <any command> <time(sec)>`,
+		Long: `# meow
+		meow is desktop toast notice tool.
+		# usage
+		### 1 default 
+		"mw --note <Note to be displayed in the notification> --after <Interval between notifications (sec)> "
+		
+		Simple desktop notification.
+		### 2 with command
+		"mw <any command> --note <Note to be displayed in the notificatio> --after <Interval between notifications of running(sec)>"
+		  
+		Desktop notification after command execution is complete.
+		## options
+		### --minute bool
+		Set interval in minutes
+		### --hour bool
+		Set interval in hour
+		### --snooze string
+		Set snooze(WIP)
+		`,
 		Args: func(cmd *cobra.Command, args []string) error {
 			var out string
 			var err error

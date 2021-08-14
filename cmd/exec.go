e62f81d3b823e2b725acf9c4faf949749c3a62dd
aucuba <aoki.070207@gmail.com>
2021-08-14 15:08:06 +0900

    Update exec.go

--- cmd/exec.go
+++ cmd/exec.go
@@ -28,15 +28,17 @@ import (
 var execCmd = &cobra.Command{
 	Use:   "exec",
 	Short: "meow! this is notifer",
-	Long: `# meow
+	Long: `
+	meow
 	meow is desktop toast notice tool.
 	Simple desktop notification.
-	### 2 with command
+	 2 with command
 	mw <any command> --note "foo bar" <Note to be displayed in the notificatio> --interval "1h3m30s" <Interval between notifications of running"1h3m30s">
 	  
 	Desktop notification after command execution is complete.
-	## options
-	### --snooze string "1h3m30s"
+	
+	options
+	--snooze string "1h3m30s"
 	Notification snooze time. Default false
 	`,
 	RunE: func(cmd *cobra.Command, args []string) error {
